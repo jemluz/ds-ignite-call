e52605efb4ffa934ee7fd73cd0255549e9748613
jemluz <contatojemima@gmail.com>
2024-07-17 18:21:53 -0300

    add: first page doc and colors tokens doc
    - npm i polished (for constrast calc)

--- packages/docs/.storybook/preview.ts
+++ packages/docs/.storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react";
+import { themes } from '@storybook/theming'
 
 const preview: Preview = {
   parameters: {
@@ -11,6 +12,9 @@ const preview: Preview = {
     backgrounds: {
       default: 'dark'
     },
+    docs: {
+      theme: themes.dark
+    }
   },
 };
 
