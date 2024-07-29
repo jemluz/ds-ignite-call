dba66b92a030d1d407d37382300e50a05912d839
jemluz <contatojemima@gmail.com>
2024-07-29 15:29:46 -0300

    add: text area component
    - create react text area component
    - export at react repo main index
    - create text area stories

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -4,6 +4,7 @@ export * from './components/Heading'
 export * from './components/Avatar'
 export * from './components/Button'
 export * from './components/TextInput'
+export * from './components/TextArea'
 
 // Another form of define props
 // export type ButtonProps = {
