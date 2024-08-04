fcabcb83239f8b4b8fba6a964e261093e8277343
jemluz <contatojemima@gmail.com>
2024-08-04 16:00:40 -0300

    update: storybook version

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -11,6 +11,7 @@ function getAbsolutePath(value: string): any {
 }
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
   addons: [
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-links"),
@@ -18,20 +19,27 @@ const config: StorybookConfig = {
     getAbsolutePath("@chromatic-com/storybook"),
     getAbsolutePath("@storybook/addon-interactions"),
     getAbsolutePath("@storybook/addon-a11y"),
+    getAbsolutePath("@storybook/addon-mdx-gfm"),
+    "@chromatic-com/storybook"
   ],
+
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+
+  docs: {},
+
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
       config.base = '/design-system-ignite-call/'
     }
 
     return config
+  },
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
   }
 };
 export default config;
